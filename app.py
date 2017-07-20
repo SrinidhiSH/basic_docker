e0d94589378f3496cadfa8c22c76f4b655a62847
SrinidhiSH <srinih@thoughtworks.com>
2017-07-20 12:50:42 +0530

    Ver 1.0

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Flask Dockerized'
+    return 'Version 1 is made here'
 
 if __name__ == '__main__':
     app.run(debug=True)
